abcc20a5f21ad90c0880f1c9eabc1674d93654c4
betoayza <aaayza@gmail.com>
2023-04-27 21:22:31 -0300

    Prepared to deploy

--- deploy.py
+++ deploy.py
@@ -30,3 +30,16 @@ print(compiled_sol)
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
     print("Codigo compilado guardado!")
+
+# Para desplegar necesito:
+# 1- Bytecode
+# 2- ABI
+
+bytecode = compiled_sol["contracts"]["SuperContract.sol"]["SuperContract"]["evm"][
+    "bytecode"
+]["object"]
+abi = json.loads(
+    compiled_sol["contracts"]["SuperContract.sol"]["SuperContract"]["metadata"]
+)["output"]["abi"]
+
+print({"bytecode": bytecode, "abi": abi}, "Listo para hacer el deploy!")
