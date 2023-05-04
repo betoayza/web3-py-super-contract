2327560fc465ebee9fbeb588feff89d187700a03
betoayza <aaayza@gmail.com>
2023-05-04 20:48:03 -0300

    Interact with deployed contract working

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ from termcolor import colored
 from dotenv import load_dotenv
 import os
 
-load_dotenv() # carga el ".env" al principio
+load_dotenv()  # carga el ".env" al principio
 
 # Extraer el codigo del contrato
 with open("./SuperContract.sol") as file:
@@ -31,11 +31,11 @@ compiled_sol = compile_standard(
     solc_version="0.8.0" "",
 )
 
-print(compiled_sol)
+# print(compiled_sol)
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
-    print("Codigo compilado guardado!")
+    print("Codigo compilado y guardado!")
 
 # Para desplegar necesito:
 ## 1- Bytecode
@@ -57,12 +57,14 @@ try:
         Web3.HTTPProvider("http://127.0.0.1:7545")
     )  # client w3 to cpnnect to ganache
 
-    my_address = "0x08a4134686c9c0a422c3eED336b63a7844A77876"
+    my_address = "0xdF9De2D0693037C194A4073D66f816aC1bdD3650"
     private_key = os.getenv("PRIVATE_KEY")  # to sign transactions
 
     # DEPLOY
-    SuperContract = w3.eth.contract(abi=abi, bytecode=bytecode) # crea una instancia del contrato
-    print(SuperContract)
+    SuperContract = w3.eth.contract(
+        abi=abi, bytecode=bytecode
+    )  # crea una instancia del contrato
+    #print(SuperContract)
 
     nonce = w3.eth.get_transaction_count(my_address)
     print("Nonce:", nonce)
@@ -81,16 +83,42 @@ try:
         }
     )
     # Contract.constructor() crea una instancia nueva del contrato en la red de ETH, necesaria para el deploy
-    print(tx, type(tx))
- 
+    # print(tx, type(tx))
+
     # 2
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
-    print(signed_tx)
-
-    # 3
-    # sended_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
     # print(signed_tx)
 
+    # 3
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    print("Generating transaction, wait...")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(tx_receipt.contractAddress)
+
+    # ----- WORKING WITH DEPLOYED CONTRACTS
+    super_contract = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+    ##funciones call y transact
+    print(
+        super_contract.functions.getNumber().call()
+    )  # llama a la funcion "retrive()" del contrato con la función "call()"
+
+    store_transaction = super_contract.functions.registerNumber(20).build_transaction(
+        {
+            "chainId": chain_id,
+            "gasPrice": w3.eth.gas_price,  # no siempre es necesario
+            "from": my_address,  # desde dónde envío la transacción
+            "nonce": nonce + 1,  # contador de transaferencias de la address
+        }
+    )
+
+    signed_store_tx = w3.eth.account.sign_transaction(
+        store_transaction, private_key=private_key
+    )
+    print("Updating register number...")
+
+    t_hash = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(t_hash)
+    print(super_contract.functions.getNumber().call())
 
 except Exception as error:
     print(colored("\nHubo un error: ", "yellow"), colored(error, "red"))
